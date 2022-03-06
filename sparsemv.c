13f40f5c0775fd2117917f80cbcb72a4ee3c96e0
Neema Raiyat <u2001540@live.warwick.ac.uk>
2022-03-06 00:54:56 +0000

    Tried using 12 threads, but processor does not support hyper threading

--- sparsemv.c
+++ sparsemv.c
@@ -21,6 +21,8 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
 
   // TODO: Optimize this file
   // TODO: Pragma in waxpby?
+  // ! Cpu is not hyperthreaded, hence 'export OMP_NUM_THREADS=6' is used
+  // ! There aren't any race conditions so far since waxpby changed with thread number yet is not even multithreaded so Dont worry about that
 
   const int nrow = (const int) A->local_nrow;
   int j = 0;
@@ -29,7 +31,7 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
       double sum = 0.0;
       const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i];
       const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i];
-      const int cur_nnz = (const int) A->nnz_in_row[i]
+      const int cur_nnz = (const int) A->nnz_in_row[i];
 
       for (j=0; j< cur_nnz; j++) {
         sum += cur_vals[j]*x[cur_inds[j]];
